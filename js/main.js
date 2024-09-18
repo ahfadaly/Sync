756196b40642bf1192ab38f9a10c421caae59ecd
ahfadaly <fadaly.dev@gmail.com>
2024-09-18 18:51:21 +0300

    remove btn

--- js/main.js
+++ js/main.js
@@ -10,37 +10,6 @@ navlinks.forEach((item) => {
   });
 });
 
-$(`document`).ready(() => {
-  // testimonials
-  $(".testimonials .owl-carousel").owlCarousel({
-    stagePadding: 250,
-    loop: true,
-    margin: 30,
-    rtl: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    dots: true,
-    nav: true,
-    navText: [
-      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-arrow-left fa-lg"></i></button>',
-      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-arrow-right fa-lg"></i></button>',
-    ],
-    responsive: {
-      0: {
-        stagePadding: 0,
-        items: 1,
-      },
-      769: {
-        stagePadding: 0,
-        items: 2,
-      },
-      1200: {
-        items: 1,
-      },
-    },
-  });
-});
-
 // AOS ANIMATION ON SCROLL
 AOS.init({
   duration: 1000,
